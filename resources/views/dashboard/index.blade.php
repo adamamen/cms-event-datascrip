7619db8362b967ff0ac6ec9e9f9a67bade85e83b
unknown <adamamen21@gmail.com>
2023-09-13 19:51:22 +0700

    change font awesome in dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -24,7 +24,7 @@
                             <div class="card card-hero">
                                 <div class="card-header">
                                     <div class="card-icon">
-                                        <i class="far fa-question-circle"></i>
+                                        <i class="fas fa-eye"></i>
                                     </div>
                                     <h4>{{ !empty($totalVisitor) ? $totalVisitor : '0' }}</h4>
                                     <a href="{{ route('visitor_event.index', ['page' => $value['title_url']]) }}">
@@ -37,7 +37,7 @@
                             <div class="card card-hero">
                                 <div class="card-header">
                                     <div class="card-icon">
-                                        <i class="far fa-question-circle"></i>
+                                        <i class="fas fa-user"></i>
                                     </div>
                                     <h4>{{ $totalAdmin }}</h4>
                                     <a href="{{ route('admin_event.index', ['page' => $value['title_url']]) }}">
@@ -52,7 +52,7 @@
                         <div class="card card-hero">
                             <div class="card-header">
                                 <div class="card-icon">
-                                    <i class="far fa-question-circle"></i>
+                                    <i class="fas fa-building"></i>
                                 </div>
                                 <h4>{{ $totalDivisi }}</h4>
                                 <a href="{{ route('company_event.index', ['page' => 'cms']) }}">
@@ -65,7 +65,7 @@
                         <div class="card card-hero">
                             <div class="card-header">
                                 <div class="card-icon">
-                                    <i class="far fa-question-circle"></i>
+                                    <i class="fas fa-calendar"></i>
                                 </div>
                                 <h4>{{ $totalEvent }}</h4>
                                 <a href="{{ route('index', ['page' => 'cms']) }}">
@@ -78,7 +78,7 @@
                         <div class="card card-hero">
                             <div class="card-header">
                                 <div class="card-icon">
-                                    <i class="far fa-question-circle"></i>
+                                    <i class="fas fa-eye"></i>
                                 </div>
                                 <h4>{{ $totalVisitor }}</h4>
                                 <a href="{{ route('visitor_event.index', ['page' => 'cms']) }}">
@@ -91,7 +91,7 @@
                         <div class="card card-hero">
                             <div class="card-header">
                                 <div class="card-icon">
-                                    <i class="far fa-question-circle"></i>
+                                    <i class="fas fa-user"></i>
                                 </div>
                                 <h4>{{ $totalAdmin }}</h4>
                                 <a href="{{ route('admin_event.index', ['page' => 'cms']) }}">
