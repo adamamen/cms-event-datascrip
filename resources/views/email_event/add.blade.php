29a0c52c799a79e7c25fd1df46d570e11558c74f
Adam Andito Firdaus <adam_firdaus@datascrip.co.id>
2024-10-23 10:23:24 +0700

    change class summernote to summernote-simple

--- resources/views/email_event/add.blade.php
+++ resources/views/email_event/add.blade.php
@@ -51,7 +51,7 @@
                                                     <label
                                                         class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
                                                     <div class="col-sm-12 col-md-7">
-                                                        <textarea class="summernote" id="content" name="content"></textarea>
+                                                        <textarea class="summernote-simple" id="content" name="content"></textarea>
                                                     </div>
                                                 </div>
                                                 <div class="form-group row mb-4">
